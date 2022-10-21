c40111959e05c5fc8b4de39dfce19a942cb96e95
ibrahim kurut <Ibrahim.kurut06@gmail.com>
2022-10-22 01:32:53 +0300

    create slider to graphic design

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+
+
+
+
+
+
+
+
+//     // gallery
+//     $(".thumbnails img").on("click", function () {
+
+//         $(this).addClass("selected").siblings().removeClass("selected");
+
+//         $(".master-img img").hide().attr("src", $(this).attr("src")).fadeIn(400);
+
+//     });
+//     // 3. ders
+//     // next right
+//     $(".master-img .fa-chevron-right").on('click', function () {
+//         $(".thumbnails .selected").next().click();
+//     });
+//     // prev left
+//     $(".master-img .fa-chevron-left").on('click', function () {
+//         $(".thumbnails .selected").prev().click();
+//     });
+
+
+
+
+
+// });
